52cddf024237bf24330f4e544c649d7ebd589543
K-Philleani <964202623@qq.com>
2021-02-20 12:04:21 +0800

    歌手List

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux-immutable'
 import { reducer as recommendReducer } from '../application/Recommend/store/index'
+import { reducer as singersReducer } from '../application/Singers/store'
 
 export default combineReducers({
-  recommend: recommendReducer
+  recommend: recommendReducer,
+  singers: singersReducer
 })
 
